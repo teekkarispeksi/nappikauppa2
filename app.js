fd22e3f0ab5e751658071327831417e86a36b00a
Arttu Voutilainen <arttu.voutilainen@gmail.com>
2015-07-26 23:27:57 +0300

    added nodejs-express server
    the server serves static html (index.html) from frontend/ and routes all
    calls to /api/ through /backend/routes.js

---
+++ app.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+
+var api = require('./backend/routes');
+
+var app = express();
+
+app.use(logger('dev'));
+
+//app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/frontend/index.html');
+});
+
+app.use('/api', api);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.send( {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.send({
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
