a4d7540da756296c6008db246aa9f74367a62101
Arttu Voutilainen <arttu.voutilainen@gmail.com>
2015-08-11 14:44:39 +0300

    add backend route to show some show information at /api/shows/:id

--- backend/routes.js
+++ backend/routes.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+var show = require('./show.js');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/shows/', function(req, res) {
+  res.json(show.getAll());
+});
+router.get('/shows/:showid', function(req, res) {
+  res.json(show.get(req.params.showid));
 });
 
 module.exports = router;
