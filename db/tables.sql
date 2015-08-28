20db67741497967861cd477056f51ed8242e994e
Arttu Voutilainen <arttu.voutilainen@gmail.com>
2015-08-28 23:50:05 +0300

    a db model that does not require entries for unnumbered seats

--- db/tables.sql
+++ db/tables.sql
@@ -33,14 +33,17 @@ create table nk2_discount_groups (
 drop table if exists nk2_orders;
 create table nk2_orders (
   `id` int(10) unsigned not null auto_increment,
-  `name` varchar(255) not null,
-  `email` varchar(255) not null,
-  `discount_code` varchar(32) not null,
-  `time` datetime not null,
-  `price` decimal(10,2) not null,
+  `name` varchar(255) default null,
+  `email` varchar(255) default null,
+  `discount_code` varchar(32) default null,
+  `time` datetime default null,
+  `price` decimal(10,2) default null,
   `payment_id` varchar(20) default NULL,
+  `reserved_until` datetime,
+  `reserved_session_id` varchar(32),
   PRIMARY KEY  (`id`),
   foreign key (discount_code) references nk2_discount_codes (code)
+  on delete cascade
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -48,54 +51,40 @@ create table nk2_orders (
 drop table if exists nk2_seats;
 create table nk2_seats (
   `id` int(11) not null auto_increment,
-  `section_id` mediumint(9) not null,
-  `seat_static_id` int(11) not null,
-  `reserved_until` datetime,
-  `reserved_session_id` varchar(32),
-  `active` boolean not null default '1',
+  `section_id` smallint(6) not null,
+  `row` smallint(6) not null,
+  `number` smallint(6) not null,
+  `x_coord` smallint(6) not null,
+  `y_coord` smallint(6) not null,
+  `bad_seat` boolean not null default '0',
   PRIMARY KEY  (`id`),
-  foreign key (section_id) references nk2_section (id),
-  foreign key (seat_static_id) references nk2_seat_static (id)
+  foreign key (section_id) references nk2_sections (id)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
-drop table if exists nk2_seats_static;
-create table nk2_seats_static (
-  `id` int(11) not null auto_increment,
-  `section_static_id` smallint(6) not null,
-  `row` smallint(6),
-  `number` smallint(6),
-  `x_coord` smallint(6),
-  `y_coord` smallint(6),
-  `bad_seat` boolean not null default '0',
+drop table if exists nk2_prices;
+create table nk2_prices (
+  `id` smallint(6) not null auto_increment,
+  `section_id` smallint(6) not null,
+  `show_id` smallint(6) not null,
+  `price` decimal(10,2) not null,
+  `active` boolean not null default '1',
   PRIMARY KEY  (`id`),
-  foreign key (section_static_id) references nk2_sections_static (id)
+  foreign key (section_id) references nk2_sections (id),
+  foreign key (show_id) references nk2_shows (id)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 drop table if exists nk2_sections;
 create table nk2_sections (
-  `id` smallint(6) not null auto_increment,
-  `section_static_id` smallint(6) not null,
-  `show_id` smallint(6) not null,
-  `price` decimal(10,2) not null,
-  `active` boolean not null default '1',
-  PRIMARY KEY  (`id`),
-  foreign key (section_static_id) references nk2_sections_static (id),
-  foreign key (show_id) references nk2_shows (id)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
-drop table if exists nk2_sections_static;
-create table nk2_sections_static (
   `id` int(10) unsigned not null auto_increment,
-  `venue_id` smallint(6) not null,
+  `venue_id` smallint(6),
   `title` varchar(255) not null,
   `row_name` varchar(20) not null,
   `sort` int(11) not null default '0',
+  `seat_count` int(11) default null,
   PRIMARY KEY  (`id`),
   foreign key (venue_id) references nk2_venues (id)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
@@ -122,7 +111,8 @@ create table nk2_tickets (
   `id` int(10) unsigned not null auto_increment,
   `order_id` int(11) unsigned not null,
   `show_id` int(11) unsigned not null,
-  `seat_id` int(11) unsigned not null,
+  `seat_id` int(11) unsigned,
+  `section_id` int(11) unsigned,
   `discount_group_id` smallint(6),
   `hash` varchar(32) not null,
   `price` decimal(10,2) not null,
@@ -131,6 +121,7 @@ create table nk2_tickets (
   foreign key (order_id) references nk2_orders (id),
   foreign key (show_id) references nk2_shows (id),
   foreign key (seat_id) references nk2_seats (id),
+  foreign key (section_id) references nk2_sections (id),
   foreign key (discount_group_id) references nk2_discount_groups (id)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
--- db/test-data.sql
+++ db/test-data.sql
@@ -14,32 +14,26 @@ insert into nk2_venues (title, ticket_type, description) values
     ('Aleksanterin teatteri', 'numbered-seats', 'Bulevardi xx, Helsinki\nRatikat y,z'),
     ('Sigyn-sali', 'generic', 'xxkatu yy, Turku');
 
-insert into nk2_sections_static (venue_id, title, row_name) values
-    (1, 'Permanto, parhaat paikat', 'riiv'),
-    (1, 'Permanto', 'rivi'),
-    (1, '1. parvi', 'rivi'),
-    (1, 'Aitiot, 1.parvi', 'aitio'),
+insert into nk2_sections (venue_id, title, row_name, seat_count) values
+    (1, 'Permanto, parhaat paikat', 'riiv', null),
+    (1, 'Permanto', 'rivi', null),
+    (1, '1. parvi', 'rivi', null),
+    (1, 'Aitiot, 1.parvi', 'aitio', null),
 
-    (2, 'Permanto', 'riiv');
+    (2, 'Permanto', 'riiv', 200);
 
-insert into nk2_seats_static (section_static_id, row, number, x_coord, y_coord, bad_seat) values
+insert into nk2_seats (section_id, row, number, x_coord, y_coord, bad_seat) values
     (1, 1, 1, 10, 10, false),
     (1, 1, 3, 30, 10, false),
     (2, 1, 2, 20, 10, false),
     (3, 5, 78, 40, 150, true),
-    (4, 5, 2, 100, 80, false),
-
-    -- generic tickets - seats not numbered
-    -- there might be a better way to do this
-    (5, null, null, null, null, false),
-    (5, null, null, null, null, false),
-    (5, null, null, null, null, false);
+    (4, 5, 2, 100, 80, false);
 
 insert into nk2_shows (title, venue_id, time, active, inactivate_time, description) values
     ('Ensi-ilta', 1, '2016-04-19 19:00:00', true, '2016-04-18 12:00:00', 'Hyviä paikkoja\ntule ajoissa paikalle\n<b>tarvitaan html-tuki</b>'),
     ('Turku', 2, '2016-04-21 19:00:00', true, '2016-04-20 12:00:00', 'Lorem ipsum');
 
-insert into nk2_sections (section_static_id, show_id, price, active) values
+insert into nk2_prices (section_id, show_id, price, active) values
     (1, 1, 20.00, true),
     (2, 1, 18.00, true),
     (3, 1, 14.00, true),
@@ -47,26 +41,16 @@ insert into nk2_sections (section_static_id, show_id, price, active) values
 
     (5, 2, 14.00, true);
 
-insert into nk2_seats (section_id, seat_static_id, reserved_until, reserved_session_id, active) values
-    (1, 1, null, null, true),
-    (1, 2, '2017-01-01', 'xyz', true),
-    (2, 3, null, null, false),
-    (3, 4, null, null, true),
-    (4, 5, null, null, true),
+insert into nk2_orders (name, email, discount_code, time, price, payment_id, reserved_until, reserved_session_id) values
+    ('Pekko I', 'pekko@example.com', null, '2015-03-13 12:47:05', 14.00, 'AASDASD', null, null),
+    ('Sponsoroitu', 'bar@example.com', 'admin-code', '2015-03-15 13:39:22', 0.00, 'WOODOO', null, null),
+    (null, null, null, '2015-03-15 13:39:22', null, null, '2017-01-01', 'xyz');
 
-    (5, 6, '2017-01-01', 'abc', true),
-    (5, 7, null, null, true), -- comment this to make it sold out
-    (5, 8, '2017-01-01', 'abc', true);
-
-
-insert into nk2_orders (name, email, discount_code, time, price, payment_id) values
-    ('Pekko I', 'pekko@example.com', null, '2015-03-13 12:47:05', 14.00, 'AASDASD'),
-    ('Sponsoroitu', 'bar@example.com', 'admin-code', '2015-03-15 13:39:22', 0.00, 'WOODOO');
-
-insert into nk2_tickets (order_id, show_id, seat_id, discount_group_id, hash, price, used_time) values
-    (1, 1, 2, null, '0123456789', 18.00, null),
-    (2, 2, 5, null, 'abcdef', 14.00, null),
-    (2, 2, 6, 2, 'free!', 0.00, null);
+insert into nk2_tickets (order_id, show_id, seat_id, section_id, discount_group_id, hash, price, used_time) values
+    (1, 1, 2, null, null, '0123456789', 18.00, null),
+    (2, 1, 3, null, null, 'abcdef', 14.00, null),
+    (2, 1, 1, null, 2, 'free!', 0.00, null),
+    (3, 2, null, 5, null, 'unnumbered', 14.00, null);
 
 
 commit;
@@ -78,10 +62,8 @@ select
     orders.*,
     shows.title, shows.time,
     venues.title,
-    seats.reserved_until, seats.reserved_session_id,
-    sections.price,
-    seats_static.row, seats_static.number, seats_static.bad_seat,
-    sections_static.title
+    seats.row, seats.number, seats.bad_seat,
+    sections.title
 from nk2_tickets tickets
     join nk2_orders orders on tickets.order_id = orders.id
 
@@ -90,6 +72,3 @@ from nk2_tickets tickets
 
     join nk2_seats seats on tickets.seat_id = seats.id
     join nk2_sections sections on seats.section_id = sections.id
-
-    join nk2_seats_static seats_static on seats.seat_static_id = seats_static.id
-    join nk2_sections_static sections_static on seats_static.section_static_id = sections_static.id;
