6daff3097da413f2ffc7d56e17b3d9c1d0663281
Arttu Voutilainen <arttu.voutilainen@gmail.com>
2015-09-25 20:22:52 +0300

    added "use strict"; -statements

--- frontend/src/js/components/Header.jsx
+++ frontend/src/js/components/Header.jsx
@@ -1,3 +1,5 @@
+"use strict";
+
 var React = require('react');
 
 var Router = require('../router.js');
--- frontend/src/js/components/Store.jsx
+++ frontend/src/js/components/Store.jsx
@@ -1,3 +1,5 @@
+"use strict";
+
 var React = require('react');
 
 var ShowSelector = require('./ShowSelector.jsx');
