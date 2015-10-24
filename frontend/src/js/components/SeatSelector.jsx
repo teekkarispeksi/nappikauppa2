5deea582907ccf544064ba4834199cdae5964824
Arttu Voutilainen <arttu.voutilainen@gmail.com>
2015-10-24 12:21:31 +0300

    move theater seat map stuff to CSS, might allow some scaling someday

--- frontend/src/js/components/SeatSelector.jsx
+++ frontend/src/js/components/SeatSelector.jsx
@@ -17,8 +17,7 @@ var SeatSelector = React.createClass({
       <div className='shopping-stage seat-selector'>
         <h2>Paikkojen valinta <small>2/5</small></h2>
         <strong>{this.props.show.get('title')}</strong>
-        <div style={{position: 'relative'}}>
-          <img src='public/img/venues/venue_1.png' />
+        <div className='theaterLayout' style={{backgroundImage: 'url(public/img/venues/venue_1.png)'}}>
           {this.props.seats.map(function(seat) {
             return <Seat seat={seat} status={seat.status} key={seat.id} onClick={this.props.active ? this.props.onSeatClicked.bind(null, seat) : null} />;
           }.bind(this))}
