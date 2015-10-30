3ab406077b60f589652b4bc3dfb079ef1291f202
Pekko Lipsanen <plipsanen@palantir.com>
2015-10-30 20:52:42 +0000

    progress bar, sold-out

--- backend/show.js
+++ backend/show.js
@@ -10,10 +10,25 @@ var show = {
   getAll: function(cb) {
     db.query('select \
         shows.*, \
+        (100.0 * reserved.seatcount / total.seatcount) as reserved_percentage, \
         prices.section_id, prices.price, \
         groups.id as discount_group_id, groups.title as discount_group_title, groups.eur as discount_group_discount \
       from nk2_shows shows \
       join nk2_prices prices on shows.id = prices.show_id \
+      join ( \
+        select show_id, count(*) seatcount \
+        from nk2_tickets tickets \
+        join nk2_orders orders on tickets.order_id = orders.id \
+        where orders.status in ("seats-reserved", "payment-pending", "paid") \
+        group by show_id \
+      ) as reserved on reserved.show_id = shows.id \
+      join ( \
+        select shows.id as show_id, count(*) seatcount \
+        from nk2_shows shows \
+        join nk2_sections sections on sections.venue_id = shows.venue_id \
+        join nk2_seats seats on seats.section_id = sections.id \
+        group by show_id \
+      ) as total on total.show_id = shows.id \
       left outer join nk2_discount_groups groups on \
         (shows.id = groups.show_id or groups.show_id is null) \
         and groups.admin_only = false \
@@ -24,7 +39,7 @@ var show = {
         }
         var grouped = _.groupBy(rows, 'id');
         var shows = _.mapObject(grouped, function(showRows) {
-          var show = _.pick(showRows[0], ['id', 'title', 'venue_id', 'time', 'active', 'inactivate_time', 'description']);
+          var show = _.pick(showRows[0], ['id', 'title', 'venue_id', 'time', 'active', 'inactivate_time', 'description', 'reserved_percentage']);
           var sections = _.groupBy(showRows, 'section_id');
           show.sections = _.mapObject(sections, function(sectionRows) {
             var basePrice = sectionRows[0].price;
