11e97784df726d2d79b24cff163bad5884da3710
Pekko Lipsanen <plipsanen@palantir.com>
2015-10-31 20:11:11 +0000

    real mail config

--- backend/mail.js
+++ backend/mail.js
@@ -4,8 +4,5 @@ var nodemailer = require('nodemailer');
 var config = require('../config/config.js');
 
 // TODO: for now gmail, because it's easiest to test with. Something real soon.
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: config.email.auth
-});
+var transporter = nodemailer.createTransport(config.email);
 module.exports = transporter;
