1f4e6e97aae89cf0266f872b6f514ca4f57aa6dc
Arttu Voutilainen <arttu.voutilainen@gmail.com>
2015-12-28 19:52:00 +0200

    simpler UI and code for unnumbered seats

--- frontend/src/js/components/TicketCountSelector.tsx
+++ frontend/src/js/components/TicketCountSelector.tsx
@@ -26,33 +26,14 @@ export default class TicketCountSelector extends React.Component<ITicketCountSel
     this.state = {error: null};
   }
 
-  onChange(section, event) {
-    var newVal = event.target.value;
+  onAdd(section, event) {
     var sectionSeatIds = _.values(this.props.venue.sections[section.id].seats).map((s) => s.id) // _.keys returns strings, we need ints
-    var sectionChosenSeatIds = _.intersection(this.props.chosenSeatIds, sectionSeatIds);
-    var current = sectionChosenSeatIds.length;
-    if (current > newVal) {
-      if (newVal < 0) {
-        this.setState({error: 'Lippujen määrä ei voi olla negatiivinen'});
-        return;
-      }
-      _.chain(this.props.chosenSeatIds)
-       .last(current - newVal)
-       .each((id) => this.props.onSeatClicked(id, section.id));
-    } else if (current < newVal) {
-      var sectionReservedSeatIds = _.intersection(this.props.reservedSeatIds, sectionSeatIds);
-      var availabeSeatsCount = sectionSeatIds.length - sectionReservedSeatIds.length;
-      if (newVal > availabeSeatsCount) {
-        this.setState({error: 'Vain ' + availabeSeatsCount + ' paikkaa on vapaana'});
-        return;
-      }
-      _.chain(sectionSeatIds)
-       .difference(this.props.chosenSeatIds)
-       .difference(this.props.reservedSeatIds)
-       .sample(newVal - current) // choose random id's to lessen the chance that another user picks the same seats
-       .each((id) => this.props.onSeatClicked(id, section.id));
-    }
-    this.setState({error: null});
+
+    _.chain(sectionSeatIds)
+     .difference(this.props.chosenSeatIds)
+     .difference(this.props.reservedSeatIds)
+     .sample(1) // choose random id to lessen the chance that another user picks the same seats
+     .each((id) => this.props.onSeatClicked(id, section.id));
   }
 
   render() {
@@ -67,30 +48,27 @@ export default class TicketCountSelector extends React.Component<ITicketCountSel
       divClass += ' disabled';
     }
 
-    // different price groups among seats in this show; discount_groups[0] is the most expensive one
-    var getBasePrice = function(section: any) {
-      return section.discount_groups[0].price;
-    };
-    var prices = _.chain(this.props.show.sections).values().map(getBasePrice).unique().value().sort().reverse();
-
     return (
       <div className={divClass}>
-        <h2>Valitse lippujen määrä <small>2/5</small></h2>
+        <h2>Lisää lippuja ostoskoriin <small>2/5</small></h2>
         <div>
           {_.values(this.props.venue.sections).map((section) => {
+            var showSection = this.props.show.sections[section.id];
+            if (!showSection) {
+              return null; // if section is set to active=0 in DB table 'nk2_prices'
+            }
+            var prices = _.pluck(showSection.discount_groups, 'price').join('/');
+
             var sectionSeatIds = _.values(this.props.venue.sections[section.id].seats).map((s) => s.id); // _.keys returns strings, we need ints
             var sectionReservedSeatIds = _.intersection(this.props.reservedSeatIds, sectionSeatIds);
-            var availabeSeatsCount = sectionSeatIds.length - sectionReservedSeatIds.length;
+            var sectionChosenSeatIds = _.intersection(this.props.chosenSeatIds, sectionSeatIds);
+            var availableSeatsCount = sectionSeatIds.length - sectionReservedSeatIds.length - sectionChosenSeatIds.length;
 
+            var title = availableSeatsCount === 1 ? '1 paikka jäljellä' : '' + availableSeatsCount + ' paikkaa jäljellä'
             return (
-              <Bootstrap.Input key={section.id}
-                label={section.section_title}
-                type='number'
-                readOnly={!this.props.active || availabeSeatsCount === 0}
-                value={this.props.chosenSeatIds.length}
-                min={0} max={availabeSeatsCount}
-                bsStyle={this.state.error ? 'error' : null}
-                onChange={this.onChange.bind(this, section)} />
+              <Bootstrap.Button key={section.id} disabled={!this.props.active || availableSeatsCount === 0} title={title} onClick={this.onAdd.bind(this, section)} >
+                <Bootstrap.Glyphicon glyph='plus' /> {section.section_title} {prices} eur
+              </Bootstrap.Button>
             );
           })}
         </div>
