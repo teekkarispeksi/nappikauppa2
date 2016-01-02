8d802cda8925f7ef9e1b12df2cd28f92bb571827
Arttu Voutilainen <arttu.voutilainen@gmail.com>
2016-01-02 21:43:49 +0200

    user 'or' instead of bitwise or

--- backend/src/discountCode.ts
+++ backend/src/discountCode.ts
@@ -6,7 +6,7 @@ import log = require('./log');
 import _ = require('underscore');
 
 export function check(code: string, user: string): Promise<any> {
-  return db.query('select (:is_admin | ((dc.use_max - count(*)) > 0)) as valid from nk2_orders o \
+  return db.query('select (:is_admin or ((dc.use_max - count(*)) > 0)) as valid from nk2_orders o \
     join nk2_discount_codes dc on dc.code = o.discount_code \
     where o.discount_code = :discount_code',
   {discount_code: code, is_admin: typeof(user) !== 'undefined'})
