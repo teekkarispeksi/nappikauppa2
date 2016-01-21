b86b53e39129c96a79d211a4fdcdcfaf824647e3
Pekko Lipsanen <plipsanen@palantir.com>
2016-01-21 10:10:53 +0400

    oops typo

--- frontend/src/js-admin/components/OrderList.tsx
+++ frontend/src/js-admin/components/OrderList.tsx
@@ -44,7 +44,7 @@ export default class OrderList extends React.Component<IOrderListProps, IOrderLi
         </tr>
           {this.state.orders.map((order) => {
             var editLink = <a href={'#orders/' + order.id}>Edit</a> ;
-            var ticketLink = order.status === 'paid' ? <a href={'admin-api/orders/' + order.id + '/tickets/.pdf'}>Liput</a> : null;
+            var ticketLink = order.status === 'paid' ? <a href={'admin-api/orders/' + order.id + '/tickets.pdf'}>Liput</a> : null;
             var paymentLink = order.status === 'payment-pending' ? <a href={order.payment_url}>Maksulinkki</a> : null;
 
             return (<tr key={order.id} className={order.status === 'paid' ? 'success' : ''}>
