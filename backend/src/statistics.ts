1712ddb8ca2712fa5c3002f6eb820fa961edee17
Arttu Voutilainen <arttu.voutilainen@gmail.com>
2016-02-21 17:37:34 +0200

    just some quick statistics as a json api endpoint, no frontend

---
+++ backend/src/statistics.ts
@@ -0,0 +1,26 @@
+'use strict';
+
+import db = require('./db');
+import log = require('./log');
+import _ = require('underscore');
+
+export function stats(): Promise<any> {
+  var stats = {tickets: null, orders: null, byShow: null, byDate: null};
+  return db.query('select sum(price) as ticket_revenue from nk2_tickets').then((res) => {
+    stats.tickets = res[0];
+  }).then(() => {
+    return db.query('select sum(price) as order_revenue from nk2_orders').then((res) => {
+      stats.orders = res[0];
+    });
+  }).then(() => { // the upper is there just to convert the DATE to a STRING
+    return db.query('select upper(date(o.time)) as date, sum(t.price) as revenue from nk2_tickets t join nk2_orders o on t.order_id = o.id group by date(o.time)').then((res) => {
+      stats.byDate = res;
+    });
+  }).then(() => {
+    return db.query('select show_id, s.title, sum(price) as revenue from nk2_tickets t join nk2_shows s on t.show_id = s.id group by show_id').then((res) => {
+      stats.byShow = res;
+    });
+  }).then(() => {
+    return stats;
+  });
+}
