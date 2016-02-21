704decce4eaa9f2bcc402e345420b3db3b685628
Arttu Voutilainen <arttu.voutilainen@gmail.com>
2016-02-21 21:29:04 +0200

    show also the amount of pending payments

--- backend/src/statistics.ts
+++ backend/src/statistics.ts
@@ -5,12 +5,16 @@ import log = require('./log');
 import _ = require('underscore');
 
 export function stats(): Promise<any> {
-  var stats = {tickets: null, orders: null, byShow: null, byDate: null};
+  var stats = {tickets: null, orders: null, pending: null, byShow: null, byDate: null};
   return db.query('select sum(price) as ticket_revenue from nk2_tickets').then((res) => {
     stats.tickets = res[0];
   }).then(() => {
     return db.query('select sum(price) as order_revenue from nk2_orders').then((res) => {
       stats.orders = res[0];
+    });
+  }).then(() => {
+    return db.query('select sum(price) as pending_revenue from nk2_orders where status = "payment-pending"').then((res) => {
+      stats.pending = res[0];
     });
   }).then(() => { // the upper is there just to convert the DATE to a STRING
     return db.query('select upper(date(o.time)) as date, sum(t.price) as revenue from nk2_tickets t join nk2_orders o on t.order_id = o.id group by upper(date(o.time))').then((res) => {
