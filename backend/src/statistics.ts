c847d6f3be033f53e7550933957a9394804bd39a
Arttu Voutilainen <arttu.voutilainen@gmail.com>
2016-03-20 21:21:49 +0200

    more statistics

--- backend/src/statistics.ts
+++ backend/src/statistics.ts
@@ -5,7 +5,7 @@ import log = require('./log');
 import _ = require('underscore');
 
 export function stats(): Promise<any> {
-  var stats = {tickets: null, orders: null, pending: null, byShow: null, byDate: null};
+  var stats = {tickets: null, orders: null, status: null, groups: null, codes: null, byShow: null, byDate: null};
   return db.query('select sum(price) as revenue, count(*) as count from nk2_tickets').then((res) => {
     stats.tickets = res[0];
   }).then(() => {
@@ -13,8 +13,23 @@ export function stats(): Promise<any> {
       stats.orders = res[0];
     });
   }).then(() => {
-    return db.query('select sum(price) as revenue, count(*) as count from nk2_orders where status = "payment-pending"').then((res) => {
-      stats.pending = res[0];
+    return db.query('select status, sum(price) as revenue, count(*) as count from nk2_orders group by status').then((res) => {
+      stats.status = res;
+    });
+  }).then(() => {
+    return db.query('select title, count(*) as count, sum(t.price) as revenue \
+        from nk2_tickets t \
+        join nk2_discount_groups dg on t.discount_group_id = dg.id \
+        join nk2_orders o  on t.order_id = o.id \
+        where o.status = "paid" group by discount_group_id').then((res) => {
+      stats.groups = res;
+    });
+  }).then(() => {
+    return db.query('select code_group, count(distinct dc.code) as created, count(o.id) as used \
+        from nk2_discount_codes dc \
+        left join nk2_orders o on o.discount_code = dc.code \
+        group by dc.code_group').then((res) => {
+      stats.codes = res;
     });
   }).then(() => { // the upper is there just to convert the DATE to a STRING
     return db.query('select upper(date(o.time)) as date, sum(t.price) as revenue, count(*) as count  \
