520eeb7dae4b45be3c93297e971716e5fb0dc6a2
Arttu Voutilainen <arttu.voutilainen@gmail.com>
2018-02-11 12:44:33 +0100

    Update deploy_tar.sh to use pm2 and cooler variables. Add CI jobs to deploy the code, let's see if that works \o/

--- deploy_tar.sh
+++ deploy_tar.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
 
-# Deploys the packaged tar ball (see build_tar.sh) somewhere.
+# Deploys the packaged tar ball (see build_build.sh and .circleci/config.yml) somewhere.
+# Usage: ./deploy_tar -f TARFILE -h HOST -d DIRECTORY [-s] [-D]
 
-echo "scp $1 $2:$3"
-scp $1 $2:$3
+while getopts f:h:d:n:D option
+do
+ case "${option}"
+ in
+ f) FILE=${OPTARG};;
+ h) HOST=${OPTARG};;
+ d) DIR=${OPTARG};;
+ n) APPNAME=${OPTARG};;
+ D) DEPLOY_ONLY=true;;
+ esac
+done
 
-if [ ! "$4" = "--deploy-only" ]
+#echo "scp $FILE $HOST:$DIR"
+echo "Uploading..."
+scp $FILE $HOST:$DIR
+
+if [ -z $DEPLOY_ONLY ]
 then
   echo "Stopping..."
-  ssh $2 "cd $3 && bin/stop.sh"
+  ssh $HOST "pm2 stop $APPNAME"
 fi
 
 echo "Cleaning old files..."
-ssh $2 "cd $3 && rm -r node_modules backend checker frontend app.js"
-echo "Unpacking $1..."
-ssh $2 "cd $3 && tar -xf $1"
+ssh $HOST "cd $DIR && rm -r node_modules backend checker frontend app.js"
+echo "Unpacking $FILE..."
+ssh $HOST "cd $DIR && tar -xf $FILE"
 
-if [ ! "$4" = "--deploy-only" ]
+if [ -z $DEPLOY_ONLY ]
 then
   echo "Starting..."
-  ssh $2 "cd $3 && bin/start.sh"
+  ssh $HOST "pm2 start $APPNAME"
 fi
