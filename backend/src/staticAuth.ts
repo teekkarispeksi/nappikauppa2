65649cc0d4213769dd1a8eb45594d97e1ff04fe2
OpaL <opal@palojarvi.fi>
2018-02-12 15:50:02 +0200

    Modify static auth algorithm

--- backend/src/staticAuth.ts
+++ backend/src/staticAuth.ts
@@ -1,30 +1,32 @@
 'use strict';
 
-var config = require('../../config/config.js');
 import log = require('./log');
-
 import _ = require('underscore');
-
 import md5 = require('md5');
+
+var config = require('../../config/config.js');
+
+interface IStaticAuthUser {
+  name: string;
+  pass: string;
+  groups: string[];
+}
 
 export function authenticate(user: string, password: string, requiredGroup: string, cb: Function) {
 
   log.info('Trying to authenticate', {user: user});
 
-  // Allows silent user check to work
-  if (requiredGroup === undefined) {
-    if (_.findWhere(config.auth.static_auth.users,
-      {name: user, pass: md5(password)}
-    )) {
-      log.info('Authentication successful');
-      return cb(true);
-    }
-  }
+  var userObject = _.findWhere<IStaticAuthUser, Object>(
+    config.auth.static.users,
+    { name: user, pass: md5(password) }
+    );
 
-  if (_.findWhere(config.auth.static_auth.users,
-    {name: user, pass: md5(password), group: requiredGroup})) {
-    log.info('Authencation successful', {user: user});
-    return cb(true);
+  if (userObject &&
+    (requiredGroup === undefined ||
+      _.contains(userObject.groups, requiredGroup)
+    )) {
+     log.info('Authencation successful', {user: user});
+     return cb(true);
   }
 
   log.warn('Access denied', {user: user, requiredGroup: requiredGroup});
