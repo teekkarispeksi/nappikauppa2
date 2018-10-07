b34ab4492500647a49332b25e78f43301cf6135b
Arttu Voutilainen <avoutilainen@palantir.com>
2018-10-07 20:16:14 +0200

    fix config copy to test frontend config correctly

--- scripts/dev_setup.sh
+++ scripts/dev_setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Copies default configuration files, if no config files are presented
-if [ ! -f config/public/frontend-sample.js ]; then
-  echo "No frontend config file presented"
+if [ ! -f config/public/frontend.js ]; then
+  echo "No frontend config file present"
   echo "Copying config/public/frontend-sample.js to config/public/frontend.js..."
   cp config/public/frontend-sample.js config/public/frontend.js
 fi
 
 if [ ! -f config/config.js ]; then
-  echo "No backend config file"
+  echo "No backend config file present"
   echo "Copying config/config-sample.js to config/config.js..."
   cp config/config-sample.js config/config.js
 fi
