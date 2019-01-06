9cf8e996a47a51058d2182745fa266ede5279fa3
Oskari Palojärvi <opal@palojarvi.fi>
2019-01-06 21:24:19 +0200

    Increase size of payment_id in table order, now fits uuid

--- db/evolutions/02_add_payment_provider_to_order.sql
+++ db/evolutions/02_add_payment_provider_to_order.sql
@@ -1,5 +1,6 @@
 START TRANSACTION;
-ALTER TABLE `nk2_orders`  ADD `payment_provider` VARCHAR(50) DEFAULT NULL;
+ALTER TABLE `nk2_orders` MODIFY COLUMN `payment_id` VARCHAR(40) DEFAULT NULL;
+ALTER TABLE `nk2_orders` ADD `payment_provider` VARCHAR(50) DEFAULT NULL;
 ALTER TABLE `nk2_orders` DROP `reserved_until`;
 ALTER TABLE `nk2_orders` DROP `reserved_session_id`;
 UPDATE `nk2_orders` SET `payment_provider` = "paytrail";
