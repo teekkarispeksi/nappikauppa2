09cbe74e3cdc992e3a94764cb639d478ec36aa98
Oskari Palojärvi <opal@palojarvi.fi>
2022-01-10 17:59:43 +0200

    Replace old winston mailgun transport with new custom implementation

--- backend/src/log.ts
+++ backend/src/log.ts
@@ -1,12 +1,50 @@
 'use strict';
 
-import winston = require('winston');
-import DailyRotateFile = require('winston-daily-rotate-file');
-// require('winston-mailgun').MailGun; // exposes winston.transports.MailGun
+import winston from 'winston';
+import DailyRotateFile from 'winston-daily-rotate-file';
+import Transport from 'winston-transport';
+import { mailer } from './mail';
+
+interface MailgunTransportOpts extends winston.transport.TransportStreamOptions {
+  targetEmail: string;
+  fromEmail: string;
+  domain: string;
+}
+
+
+/**
+ * Simple transport to send log messages via email
+ * use to
+ */
+class MailgunTransport extends Transport {
+
+  private targetEmail: string;
+  private fromEmail: string;
+  private domain: string;
+
+  constructor(opts: MailgunTransportOpts) {
+    super(opts);
+
+    this.targetEmail = opts.targetEmail;
+    this.domain = opts.domain;
+    this.fromEmail = opts.fromEmail;
+  }
+
+  log(info, cb) {
+    mailer.messages.create(this.domain, {
+      from: this.fromEmail,
+      to: this.targetEmail,
+      subject: 'NK2 - Error',
+      text: `Error: \n\n ${JSON.stringify(info)} \n\n from nappikauppa2`,
+    })
+    .then(() => cb())
+    .catch(() => cb());
+  }
+}
 
 var config = require('../../config/config.js');
 
-var transports = [
+var transports: winston.transport[] = [
   new winston.transports.Console(),
   new DailyRotateFile({
     filename: 'log/nk2.log'
@@ -14,19 +52,18 @@ var transports = [
   new DailyRotateFile({
     filename: 'log/nk2-error.log',
     level: 'error',
-    //handleExceptions: true,
+    handleExceptions: true,
   })
 ];
 
-// if (config.email.errors_to) {
-//   transports.push(new (winston.transports.MailGun)({
-//     level: 'error',
-//     to: config.email.errors_to,
-//     from: config.email.errors_from,
-//     apiKey: config.email.mailgun.api_key,
-//     domain: config.email.mailgun.domain
-//   }));
-// }
+if (config.email.errors_to) {
+  transports.push(new MailgunTransport({
+    level: 'error',
+    targetEmail: config.email.errors_to,
+    fromEmail: config.email.errors_from,
+    domain: config.email.mailgun.domain
+  }));
+}
 
 var logger = winston.createLogger({level: 'info', transports: transports});
 
